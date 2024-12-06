863fb8a74c2969ca18f9259b27c05b25f2875ac8
anup199 <anupsingh6625@gmail.com>
2024-12-06 08:29:46 +0530

    circle shape done

---
+++ 10_Dom/02_styling/style.js
@@ -0,0 +1,9 @@
+
+function circle()
+{
+    let div = document.getElementById('circle')
+    div.style.border="2px solid red";
+    div.style.height="200px"
+    div.style.width="200px"
+    div.style.borderRadius="100%"
+}
