be00f9129d56628c4bfcd1ba2a84cae7cab36238
anup199 <anupsingh6625@gmail.com>
2024-12-10 08:45:58 +0530

    bublling and capturing

--- 10_Dom/02_styling/style.js
+++ 10_Dom/02_styling/style.js
@@ -1,9 +1,29 @@
+// method 1
+// function circle()
+// {
+//     let div = document.getElementById('circle')
+//     div.style.border="2px solid red";
+//     div.style.height="200px"
+//     div.style.width="200px"
+//     div.style.borderRadius="100%"
+//     div.style.backgroundColor="green"
 
-function circle()
-{
+//     // div.style.cssText=`border:2px solid red;height:200px;width:200px`;
+// }
+
+
+// method 2
+// to get the button here
+let circle1 = document.getElementById('circle_btn')
+
+// add event at button and css properties
+circle1.addEventListener('click',()=>{
+
     let div = document.getElementById('circle')
     div.style.border="2px solid red";
     div.style.height="200px"
     div.style.width="200px"
     div.style.borderRadius="100%"
-}
+    div.style.backgroundColor="green"
+
+})
