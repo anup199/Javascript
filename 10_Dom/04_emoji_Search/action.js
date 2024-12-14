7b9d1ece350eb51e7efe87e1187368c9654019fc
anup199 <anupsingh6625@gmail.com>
2024-12-14 15:23:55 +0530

    number guessing game

--- 10_Dom/04_emoji_Search/action.js
+++ 10_Dom/04_emoji_Search/action.js
@@ -46,3 +46,81 @@ let btn = document.getElementById('btn');
 btn.addEventListener('click',()=>{
     emo_name(emojiObject)
 })
+
+let imo = [
+    {
+        "slug": "smileys-emotion",
+        "subCategories": {
+            "face-smiling": "😊",
+            "face-affection": "🥰",
+            "face-tongue": "😋",
+            "face-hand": "🤚",
+            "face-neutral-skeptical": "🤨",
+            "face-sleepy": "😴",
+            "face-unwell": "🤒",
+            "face-hat": "🎩",
+            "face-glasses": "👓",
+            "face-concerned": "😟",
+            "face-negative": "🙁",
+            "face-costume": "🎭",
+            "cat-face": "🐱",
+            "monkey-face": "🐵",
+            "emotion": "🙂"
+        }        
+    },
+    {
+        "slug": "people-body",
+        "subCategories": {
+            "hand-fingers-open": "✋",
+            "hand-fingers-partial": "🖐️",
+            "hand-single-finger": "☝️",
+            "hand-fingers-closed": "✊",
+            "hands": "👋",
+            "hand-prop": "🖖",
+            "body-parts": "🦶",
+            "person": "👤",
+            "person-gesture": "🙆‍♂️",
+            "person-role": "👩‍⚖️",
+            "person-fantasy": "🧙‍♂️",
+            "person-activity": "🏃‍♀️",
+            "person-sport": "🏀",
+            "person-resting": "🛏️",
+            "family": "👨‍👩‍👧‍👦",
+            "person-symbol": "⚧"
+        }        
+    },
+    {
+        "slug": "component",
+        "subCategories":{
+            "skin-tone": "🏽",
+            "hair-style": "💇‍♂️"
+        }        
+    },
+    {
+        "slug": "animals-nature",
+        "subCategories": {
+            "animal-mammal": "🐘",
+            "animal-bird": "🐦",
+            "animal-amphibian": "🐸",
+            "animal-reptile": "🐍",
+            "animal-marine": "🐠",
+            "animal-bug": "🐞",
+            "plant-flower": "🌸",
+            "plant-other": "🌱"
+        }        
+    },
+    {
+        "slug": "food-drink",
+        "subCategories": {
+            "food-fruit": "🍎",
+            "food-vegetable": "🥕",
+            "food-prepared": "🍽️",
+            "food-asian": "🍣",
+            "food-marine": "🐟",
+            "food-sweet": "🍰",
+            "drink": "🥤",
+            "dishware": "🍽️"
+        }        
+    }
+]
+console.log(imo[0].subCategories.emotion);
